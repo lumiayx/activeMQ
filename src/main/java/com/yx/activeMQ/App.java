367cd6d09d15d6ac6ed3e07df1101c8fdf60c9ee
yinx <906919789@qq.com>
2017-09-21 18:47:53 +0800

    提交maven

--- src/main/java/com/yx/activeMQ/App.java
+++ src/main/java/com/yx/activeMQ/App.java
@@ -5,5 +5,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
     }
 }
